b1ffc638a52b9ed773b5ef33bf2fe1d975a05d45
Emanuele Serafini <ema.serafini@gmail.com>
2016-05-30 09:32:35 +0200

    I18n setup

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -54,4 +54,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  #  Use t() instead of I18n.t() in all of your tests
+  config.include AbstractController::Translation
 end
