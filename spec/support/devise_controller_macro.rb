853ce38e1d316215ee2f620976781cd58aa283ac
Emanuele Serafini <ema.serafini@gmail.com>
2016-06-11 00:07:01 +0200

    remove spec files

--- spec/support/devise_controller_macro.rb
+++
@@ -1,8 +0,0 @@
-module DeviseControllerMacros
-  def sign_in user = nil
-    before(:each) do
-      @request.env['devise.mapping'] = Devise.mappings[:user]
-      sign_in(user || create(:confirmed_user))
-    end
-  end
-end
