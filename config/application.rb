f1d2c5dc60628e832d82551cfdbd335108c92a79
Emanuele Serafini <ema.serafini@gmail.com>
2016-09-02 21:53:56 +0200

    code arrangement

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,6 @@ module Kengai
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.autoload_paths += Dir[Rails.root.join('app', 'models', 'sensors')]
+    config.autoload_paths += Dir[Rails.root.join('app', 'models', '{*/}')]
   end
 end
