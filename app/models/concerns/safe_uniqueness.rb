40f5a8301a66ab1bf049093d2bbc249208db4f31
Emanuele Serafini <ema.serafini@gmail.com>
2016-10-07 09:53:03 +0200

    move safe save uniqueness into concern

---
+++ app/models/concerns/safe_uniqueness.rb
@@ -0,0 +1,25 @@
+module SafeUniqueness
+  extend ActiveSupport::Concern
+
+  included do
+    around_save :save_managing_uniqueness,
+      if: proc { |model| UNIQUE_FIELDS.map{ |field| model.send("#{field}_changed?") }.include?(true) }
+  end
+
+  def save_managing_uniqueness
+    attempts ||= 0
+    self.class.connection.execute('SAVEPOINT before_record_create;')
+    yield
+    self.class.connection.execute('RELEASE SAVEPOINT before_record_create;')
+  rescue ActiveRecord::RecordNotUnique => error
+    attempts += 1
+    if attempts < 3
+      duplicated_field = error.message[/Key \((\w+)\)/, 1]
+      send("assign_#{duplicated_field}", true) if duplicated_field && UNIQUE_FIELDS.include?(duplicated_field)
+      self.class.connection.execute('ROLLBACK TO SAVEPOINT before_record_create;')
+      retry
+    else
+      raise error, 'Retries exhausted'
+    end
+  end
+end
--- app/models/thermostat.rb
+++ app/models/thermostat.rb
@@ -5,20 +5,17 @@ class Thermostat < ApplicationRecord
   has_many :users, through: :subscribers
   belongs_to :temperature, class_name: 'Temperature', dependent: :destroy
   belongs_to :humidity, class_name: 'Humidity', dependent: :destroy
-  has_one :manual_program, dependent: :destroy, inverse_of: :thermostat
 
   enum status: { standby: 0, unknown: 1, heating: 2, cooling: 3 }
-  enum program_status: { manual: 0 } # auto: 1
+  enum program_status: { manual: 0, auto: 1 }
 
   before_validation :set_default_values, on: :create
   before_save :assign_uuid, :assign_access_token
-  around_save :save_managing_uniqueness,
-    if: proc { |thermostat| UNIQUE_FIELDS.map{ |field| thermostat.send("#{field}_changed?") }.include?(true) }
   before_update :update_started_at, if: :status_changed?
 
   validates :name, :status, :program_status, presence: true
 
-  accepts_nested_attributes_for :temperature, :humidity, :manual_program
+  accepts_nested_attributes_for :temperature, :humidity
 
   def to_param
     uuid
@@ -43,23 +40,6 @@ class Thermostat < ApplicationRecord
     self.enabled = false unless enabled?
     self.status         ||= :unknown
     self.program_status ||= :manual
-  end
-
-  def save_managing_uniqueness
-    attempts ||= 0
-    self.class.connection.execute('SAVEPOINT before_record_create;')
-    yield
-    self.class.connection.execute('RELEASE SAVEPOINT before_record_create;')
-  rescue ActiveRecord::RecordNotUnique => error
-    attempts += 1
-    if attempts < 3
-      duplicated_field = error.message[/Key \((\w+)\)/, 1]
-      send("assign_#{duplicated_field}", true) if duplicated_field && UNIQUE_FIELDS.include?(duplicated_field)
-      self.class.connection.execute('ROLLBACK TO SAVEPOINT before_record_create;')
-      retry
-    else
-      raise error, 'Retries exhausted'
-    end
   end
 
   def assign_uuid(force = false)
